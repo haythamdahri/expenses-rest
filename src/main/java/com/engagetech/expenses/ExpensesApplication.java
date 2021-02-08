118deb7f266605691f3303b206d116e5fedae992
haythamdahri <haytham.dahri@gmail.com>
2021-02-08 21:48:16 +0100

    Initial Commit

---
+++ src/main/java/com/engagetech/expenses/ExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.engagetech.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engagetech/expenses/ExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engagetech.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
